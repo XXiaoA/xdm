2f188c33ba8da6c30c6fc9b1674bde2eaae3a349
XXiaoA <isxxiaoa@gmail.com>
2022-08-29 16:53:19 +0800

    fix!: don't have to have `create` option now * use serde_yaml::Value instead of struct to read yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use colored::Colorize;
-use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, fs, path::Path};
+use serde_yaml::Value;
+use std::{fs, path::Path};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -11,58 +11,67 @@ struct Args {
     file: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    create: Vec<String>,
-    link: HashMap<String, HashMap<String, String>>,
+fn get_conf() -> Value {
+    // get file path from user
+    let args = Args::parse();
+    let file_path = &args.file;
+
+    let file_content = std::fs::File::open(file_path).expect("Could not open file.");
+    let config: Value = serde_yaml::from_reader(file_content).expect("Could not read values.");
+    config
 }
 
-impl Config {
-    fn get_conf() -> Config {
-        // get file path from user
-        let args = Args::parse();
-        let file_path = &args.file;
-
-        let file_content = std::fs::File::open(file_path).expect("Could not open file.");
-        let config: Config = serde_yaml::from_reader(file_content).expect("Could not read values.");
-        config
-    }
-
-    fn get_link_parameter(&self, original: &str, parameter: &str) -> &str {
-        let all_links = &self.link;
-        let all_parameters = all_links.get(original).unwrap();
-        let value = all_parameters
-            .get(parameter)
-            .map(String::as_str)
-            .unwrap_or_else(|| match parameter {
-                "exist" | "if" | "create" => "true",
-                "force" => "false",
-                _ => "",
-            });
-        value
-    }
+fn get_link_parameter(conf: Value, original: String, parameter: String) -> String {
+    let all_links = conf.get("link").unwrap();
+    let all_parameters = all_links.get(original).unwrap();
+    let _value = all_parameters.get(&parameter);
+    let value = match _value {
+        Some(v) => v.as_str().unwrap_or("").to_owned(),
+        None => match parameter.as_str() {
+            "exist" | "if" | "create" => "true".to_string(),
+            "force" => "false".to_string(),
+            _ => "".to_string(),
+        },
+    };
+    value
 }
 
 fn main() {
-    let xdm_config = Config::get_conf();
+    let xdm_config = get_conf();
+
     // do jobs for create
-    println!("######job#######");
-    let all_creates = &xdm_config.create;
-    for directory in all_creates {
-        let path = absolute_path(directory);
-        let path = Path::new(&path);
-        if !path.exists() {
-            fs::create_dir_all(path).unwrap();
-            println!("{}{}", directory.green(), ": created the directory successfully".green())
+    let all_creates = xdm_config.get("create");
+
+    if let Some(..) = all_creates {
+        println!("######create######");
+        let all_creates = all_creates.unwrap().as_sequence().unwrap();
+
+        for dir in all_creates {
+            let dir = dir.as_str().unwrap();
+            let path = absolute_path(dir);
+            let path = Path::new(&path);
+            if !path.exists() {
+                fs::create_dir_all(path).unwrap();
+                println!(
+                    "{}{}",
+                    dir.green(),
+                    ": created the directory successfully".green()
+                )
+            }
         }
     }
 
     // do jobs for link
-    println!("######link######");
-    let all_links = &xdm_config.link;
-    for link_item in all_links {
-        let original = link_item.0;
-        let link = xdm_config.get_link_parameter(original, "path");
+    println!("#######link#######");
+    let all_link_items = xdm_config.get("link").unwrap().as_mapping().unwrap();
+    for link_item in all_link_items {
+        let original = link_item.0.as_str().unwrap();
+        let link = get_link_parameter(
+            xdm_config.to_owned(),
+            original.to_string(),
+            "path".to_string(),
+        );
+
         if link.is_empty() {
             println!(
                 "{}{}",
@@ -70,19 +79,22 @@ fn main() {
                 ": something wrong in `path`".red()
             )
         } else {
-            match create_softlink(original, link) {
+            match create_softlink(original, link.as_str()) {
                 Ok(_) => {
                     // TODO: make the following code cleaner
                     // create the parent directory if need
-                    let link_path = Path::new(link);
-                    let create: bool = xdm_config
-                        .get_link_parameter(original, "create")
-                        .parse()
-                        .unwrap();
+                    let link_path = Path::new(&link);
+                    let create: bool = get_link_parameter(
+                        xdm_config.to_owned(),
+                        original.to_string(),
+                        "create".to_string(),
+                    )
+                    .parse()
+                    .unwrap();
                     let link_parent = link_path.parent().unwrap();
                     if create && !link_parent.exists() {
                         fs::create_dir_all(link_parent.to_str().unwrap()).unwrap();
-                        create_softlink(original, link).unwrap();
+                        create_softlink(original, link.as_str()).unwrap();
                     }
                     println!("{} {} {}", original.green(), "=>".green(), link.green());
                 }
@@ -122,15 +134,15 @@ fn absolute_path(path: &str) -> String {
     use path_absolutize::Absolutize;
     use std::env;
 
-    let path_vec: Vec<&str> = path.split("/").collect();
+    let path_vec: Vec<&str> = path.split('/').collect();
 
     if path_vec[0] == "~" {
-        let prev = env::var("HOME").unwrap_or("none".to_string());
+        let prev = env::var("HOME").unwrap_or_else(|_| "none".to_string());
         let end = path_vec[1..].join("/");
         format!("{}/{}", prev, end)
     } else if &path_vec[0][0..1] == "$" {
         let env = &path_vec[0][1..];
-        let prev = env::var(env).unwrap_or("none".to_string());
+        let prev = env::var(env).unwrap_or_else(|_| "none".to_string());
         let end = path_vec[1..].join("/");
         format!("{}/{}", prev, end)
     } else {
@@ -148,21 +160,27 @@ fn create_softlink(original: &str, link: &str) -> Result<(), String> {
     let original_path = Path::new(absolute_original);
     let link_path = Path::new(absolute_link);
 
-    let xdm_config = Config::get_conf();
-    let exist: bool = xdm_config
-        .get_link_parameter(original, "exist")
-        .parse()
-        .unwrap();
-    let force: bool = xdm_config
-        .get_link_parameter(original, "force")
-        .parse()
-        .unwrap();
-    let condition = xdm_config.get_link_parameter(original, "if");
+    let xdm_config = get_conf();
+    let exist: bool = get_link_parameter(
+        xdm_config.to_owned(),
+        original.to_string(),
+        "exist".to_string(),
+    )
+    .parse()
+    .unwrap();
+    let force: bool = get_link_parameter(
+        xdm_config.to_owned(),
+        original.to_string(),
+        "force".to_string(),
+    )
+    .parse()
+    .unwrap();
+    let condition = get_link_parameter(xdm_config, original.to_string(), "if".to_string());
 
     let command_status = if condition == "true" {
         true
     } else {
-        get_command_status(condition)
+        get_command_status(condition.as_str())
     };
 
     if !command_status {
